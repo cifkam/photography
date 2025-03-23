e5abab8bd90ad17468cbbf5296ddfde76950c547
Martin <martin.cifka@gmail.com>
2025-03-23 22:32:42 +0100

    fix

--- assets/js/preview-gallery.js
+++ assets/js/preview-gallery.js
@@ -17,19 +17,23 @@ function setupPreviewGalleryGalleryV2(id, href, images) {
   let prev = document.createElement("span");
   prev.classList.add("preview-gallery-prev");
   prev.innerHTML = "↩";
-  prev.onclick = () => {
-    g.index = Math.max(0, g.index - 1);
-    image.style.backgroundImage = "url('" + g.images[g.index] + "')";
-  };
+  prev.onclick = (function(g, image) {
+    return function() {
+        g.index = Math.max(0, g.index - 1);
+        image.style.backgroundImage = "url('" + g.images[g.index] + "')";
+    };
+  })(g, image);
+
   g.appendChild(prev);
 
   // Create next button
   let next = document.createElement("span");
   next.classList.add("preview-gallery-next");
   next.innerHTML = "↪";
-  next.onclick = () => {
-    g.index = Math.min(g.images.length - 1, g.index + 1);
-    image.style.backgroundImage = "url('" + g.images[g.index] + "')";
-  };
-  g.appendChild(next);
+  next.onclick = (function(g, image) {
+    return function() {
+        g.index = Math.min(g.images.length - 1, g.index + 1);
+        image.style.backgroundImage = "url('" + g.images[g.index] + "')";
+    };
+  })(g, image);
 }
