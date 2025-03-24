4dd05fe04dca3e7d31ecd713fcd411d08a1046ed
cifkam <martin.cifka@gmail.com>
2025-03-24 18:43:50 +0000

    Deploying to gh-pages from @ cifkam/photography@8e811f6f3102c9ab2476badaad235501db576a0e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,12 +37,68 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/photography/geometry";
           },
+        },{id: "nav-orthochromasia",
+          title: "orthochromasia",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photography/orthochromasia";
+          },
+        },{id: "nav-genius-loci",
+          title: "genius loci",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photography/genius-loci";
+          },
         },{id: "nav-suburbs",
           title: "suburbs",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/photography/suburbs";
+          },
+        },{id: "nav-landscape",
+          title: "landscape",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photography/landscape";
+          },
+        },{id: "nav-still-life-1",
+          title: "still life 1",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photography/still1";
+          },
+        },{id: "nav-still-life-2",
+          title: "still life 2",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photography/still2";
+          },
+        },{id: "nav-greenhouse",
+          title: "greenhouse",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photography/greenhouse";
+          },
+        },{id: "nav-head",
+          title: "head",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photography/head";
+          },
+        },{id: "nav-my-film-is-not-advancing",
+          title: "my film is not advancing",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photography/my-film-is-not-advancing";
           },
         },{id: "nav-test",
           title: "test",
